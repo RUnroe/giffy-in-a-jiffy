5ce18259b13c54782b42b35c227b0fdc82009b46
RUnroe <ryanunroe@gmail.com>
2021-04-12 17:39:31 -0600

    Create base of Giphy component

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-function Test() {
+const API_KEY = "Tyiskjc69fbO2GtmKoB2xUd0AqHxAl4k";
+const LIMIT = 10;
+//Limit is exclusive
+const getRandomNumber = limit => {
+  return Math.floor(Math.random() * limit);
+}
+
+const Giphy = ({search}) => {
+  const [imgSrc, setImgSrc] = React.useState("");
+
+  const searchForGif = evt => {
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=${LIMIT}&q=${search}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      setImgSrc(json[getRandomNumber(json.length)]);
+    });
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Image source={imgSrc}/>
+      <Text>Powered By GIPHY</Text>
+    </View>
+  )
+}
+
+const App = () => {
+  return (
+    <View>
+      <Giphy search="coding" />
+      <Giphy search="fun" />
+      <Giphy search="puppy" /> 
     </View>
   );
 }
-
-
- class App extends Component {
-}
-
 
 export default App;
